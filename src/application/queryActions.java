8f188067c7d443d4b3485dd7502c562294178627
preston-bied <106128563+preston-bied@users.noreply.github.com>
2022-06-16 19:08:41 +0200

    restock report v1

--- src/application/queryActions.java
+++ src/application/queryActions.java
@@ -151,4 +151,88 @@ public class queryActions {
     }
     return output;
     }
+
+    public static String[] restockReport(String startDate, String endDate) {
+        Connection conn = null;
+        String teamNumber = "2";
+        String sectionNumber = "950";
+        String dbName = "csce315" + sectionNumber + "_" + teamNumber + "db";
+        String dbConnectionString = "jdbc:postgresql://csce-315-db.engr.tamu.edu/" + dbName;
+        dbSetup myCredentials = new dbSetup(); 
+
+        String sql = "SELECT saleInvoiceID FROM saleInvoiceHistory WHERE saleDate >= '" + startDate + "' AND saleDate <= '" + endDate + "'";
+        String[] output = new String[4];
+        
+        // connect to database
+        try {
+            conn = DriverManager.getConnection(dbConnectionString, dbSetup.user, dbSetup.pswd);
+            
+            Statement statement = conn.createStatement();
+            ResultSet queryOutput = statement.executeQuery(sql);
+
+            Vector<String> invoiceIDs = new Vector<>();
+            while (queryOutput.next()) {
+                invoiceIDs.addElement(queryOutput.getString("saleInvoiceID"));
+            }
+
+            Vector<String> productIDs = new Vector<>();
+            Vector<String> quantitiesSold = new Vector<>();
+            for (int i = 0; i < invoiceIDs.size(); i++) {
+                sql = "SELECT productID, quantitySold FROM saleLineItems WHERE saleInvoiceID = " + invoiceIDs.elementAt(i);
+                queryOutput = statement.executeQuery(sql);
+                while (queryOutput.next()) {
+                    productIDs.addElement(queryOutput.getString("productID"));
+                    quantitiesSold.addElement(queryOutput.getString("quantitySold"));
+                }
+            }
+
+            HashMap<String, Double> totalQuantitiesSold = new HashMap<>();
+            for (int i = 0; i < productIDs.size(); i++) {
+                totalQuantitiesSold.put(productIDs.elementAt(i), 0.0);
+            }
+            for (int i = 0; i < productIDs.size(); i++) {
+                Double quantity = Double.parseDouble(quantitiesSold.elementAt(i));
+                totalQuantitiesSold.put(productIDs.elementAt(i), totalQuantitiesSold.get(productIDs.elementAt(i)) + quantity);
+            }
+
+            Vector<String> excessProductIDs = new Vector<>();
+            Vector<String> excessProductNames = new Vector<>();
+            Vector<String> excessProductCurrent = new Vector<>();
+            Vector<String> excessProductDesired = new Vector<>();
+            for (int i = 0; i < productIDs.size(); i++) {
+                sql = "SELECT productName, currentStock, desiredStock FROM products WHERE productID = " + productIDs.elementAt(i);
+                queryOutput = statement.executeQuery(sql);
+                while (queryOutput.next()) {
+                    Double currStock = Double.parseDouble(queryOutput.getString("currentStock"));
+                    if (currStock < totalQuantitiesSold.get(productIDs.elementAt(i))) {
+                        excessProductIDs.addElement(productIDs.elementAt(i));
+                        excessProductNames.addElement(queryOutput.getString("productName"));
+                        excessProductCurrent.addElement(queryOutput.getString("currentStock"));
+                        excessProductDesired.addElement(queryOutput.getString("desiredStock"));
+                    }
+                }
+            }
+
+            String excessIDs = "";
+            String excessNames = "";
+            String excessCurrent = "";
+            String excessDesired = "";
+            for (int i = 0; i < excessProductIDs.size(); i++) {
+                excessIDs += excessProductIDs.elementAt(i) + "\n";
+                excessNames += excessProductNames.elementAt(i) + "\n";
+                excessCurrent += excessProductCurrent.elementAt(i) + "\n";
+                excessDesired += excessProductDesired.elementAt(i) + "\n";
+            }
+
+            output[0] = excessIDs;
+            output[1] = excessNames;
+            output[2] = excessCurrent;
+            output[3] = excessDesired;
+    } catch ( Exception e ) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+    }
+    return output;
+    }
 }
